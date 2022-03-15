b599b4a8a6ff64395b9452883f5e240a70eec550
Team <97787042+TeamApp2022@users.noreply.github.com>
2022-03-16 00:19:33 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,4 +24,30 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    //firebase...................................
+    // Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: 'AIzaSyDmw8-nulY88ZZp9y8bhAEEoYeibzvA3m4',
+  authDomain: '### FIREBASE AUTH DOMAIN ###',
+  projectId: 'semsar-21596'
+});
+
+var db = firebase.firestore();
+    
+ //Add Data...............................
+    db.collection("questions_team").add({
+    name: "Ada",
+    mail: "Lovelace",
+    phone: 1815,
+    message: "Lovelace",
+    
+})
+.then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
+});
+
+    
 });
